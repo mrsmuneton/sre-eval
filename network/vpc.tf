44473ba9f3a38972654680e99078054a5e3ddb08
Ray <mrs.muneton@gmail.com>
2018-08-24 07:29:52 -0700

    Create ECS cluster

--- network/vpc.tf
+++ network/vpc.tf
@@ -12,16 +12,72 @@ resource "aws_vpc" "main" {
   }
 }
 
-variable "public_subnet_cidr" {
-    description = "CIDR for the Public Subnet"
-    default = "10.0.0.0/24"
-}
-
-variable "private_subnet_cidr" {
-    description = "CIDR for the Private Subnet"
-    default = "10.0.1.0/24"
-}
-
 resource "aws_internet_gateway" "default" {
  vpc_id = "${aws_vpc.main.id}"
+ tags {
+   Name = "default-ig"
+ }
+}
+
+resource "aws_route_table" "public-route-table" {
+  vpc_id = "${aws_vpc.main.id}"
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.default.id}"
+  }
+  tags {
+    Name = "public-route-table"
+  }
+}
+
+resource "aws_subnet" "public-subnet" {
+  vpc_id     = "${aws_vpc.main.id}"
+  cidr_block = "10.0.1.0/24"
+
+  tags {
+    Name = "Main"
+  }
+}
+
+resource "aws_route_table_association" "public-route-table-assocation" {
+  subnet_id = "${aws_subnet.public-subnet.id}"
+  route_table_id = "${aws_route_table.public-route-table.id}"
+}
+
+resource "aws_subnet" "private-subnet" {
+  vpc_id     = "${aws_vpc.main.id}"
+  cidr_block = "10.0.1.0/24"
+
+  tags {
+    Name = "Main"
+  }
+}
+
+resource "aws_ecs_cluster" "foo" {
+  name = "armstrong"
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-trusty-14.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "web" {
+  ami           = "${data.aws_ami.ubuntu.id}"
+  instance_type = "t2.micro"
+
+  tags {
+    Name = "web-node"
+  }
 }
